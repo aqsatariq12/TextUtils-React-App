85173e4e639fd6ecacefc00b24698f000725b627
Aqsa tariq <aqsatariq2452003@gmail.com>
2025-02-16 00:50:33 -0800

    Updates

---
+++ precache-manifest.0f1b01f6d82b7f62347b58b4d19d14c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f6c495c9db80b3be9dc9327eb237234",
+    "url": "/TextUtils-React-App/index.html"
+  },
+  {
+    "revision": "c1ef31510b11420c2a0a",
+    "url": "/TextUtils-React-App/static/css/main.8a172173.chunk.css"
+  },
+  {
+    "revision": "0da3a3a7caac8e2de5c7",
+    "url": "/TextUtils-React-App/static/js/2.a5b0c296.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/TextUtils-React-App/static/js/2.a5b0c296.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b82bfd9cfb7415d2616f",
+    "url": "/TextUtils-React-App/static/js/3.17a77b33.chunk.js"
+  },
+  {
+    "revision": "c1ef31510b11420c2a0a",
+    "url": "/TextUtils-React-App/static/js/main.50d2919d.chunk.js"
+  },
+  {
+    "revision": "81ed5238e224c5ebffbe",
+    "url": "/TextUtils-React-App/static/js/runtime-main.2b46bc69.js"
+  }
+]);
